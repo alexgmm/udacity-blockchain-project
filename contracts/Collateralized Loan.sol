fc9be42ee8b33cfc7e7d51dbe2682af69998e0d4
alexgmm <alexandre.guedes.martins@gmail.com>
2026-02-22 17:45:27 -0300

    fix: funding/repaying and collateral claiming rule

--- contracts/Collateralized Loan.sol
+++ contracts/Collateralized Loan.sol
@@ -32,11 +32,10 @@ contract CollateralizedLoan {
         _;
     }
 
-    // Function to deposit collateral and request a loan
     function depositCollateralAndRequestLoan(uint _interestRate, uint _duration) external payable {
         require(msg.value > 0, "A collateral must be provided");
 
-        // The assignment asked to consider loan === collateral
+        // The assignment asked to consider loan == collateral
         uint loanAmount = msg.value;
         nextLoanId++;
         loans[nextLoanId] = Loan(nextLoanId, msg.sender, address(0), loanAmount, _interestRate, block.timestamp + _duration, false, false, false);
@@ -54,7 +53,9 @@ contract CollateralizedLoan {
         loan.lender = msg.sender;
 
         address payable borrower = payable(loan.borrower);
-        (bool success, ) = borrower.call{value: loan.loanAmount}("");
+        
+        // We pass the msg.value (the ETH just received) directly to the borrower
+        (bool success, ) = borrower.call{value: msg.value}("");
         require(success, "It was not possible to fund the loan");
 
         emit LoanFunded(loan.lender, loan.borrower, loan.loanAmount);
@@ -67,7 +68,8 @@ contract CollateralizedLoan {
         require(!loan.isRepaid, "This loan was already repaid");
 
         // Since Solidity hasn't float, I'm assuming interestRate is an integer
-        uint valueToPay = (loan.loanAmount * loan.interestRate)/100;
+        uint valueToPay = loan.loanAmount + (loan.loanAmount * loan.interestRate)/100;
+        loan.isRepaid = true;
 
         address payable lender = payable(loan.lender);
         (bool success, ) = lender.call{value: valueToPay}("");
@@ -80,7 +82,8 @@ contract CollateralizedLoan {
         Loan storage loan = loans[loanId];
 
         require(loan.lender == msg.sender, "You're not the lender of this loan");
-        require(block.timestamp <= loan.dueDate, "This loan is not due to claim the collateral");
+        require(block.timestamp > loan.dueDate, "This loan is not due to claim the collateral");
+        require(!loan.wasCollateralClaimed, "This loan's collateral was already claimed");
 
         uint collateral = loan.loanAmount;
         loan.wasCollateralClaimed = true;
